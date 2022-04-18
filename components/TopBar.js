befcaf983e2958e1d7b3a8ccba61b2441227cf79
valfur03 <valentin.furmanek@gmail.com>
2022-04-18 18:15:56 +0200

    feat: add the My Projects category to the top bar

--- components/TopBar.js
+++ components/TopBar.js
@@ -7,8 +7,8 @@ export default function TopBar() {
 			<ul className={styles.sections_list}>
 				<li className={styles.section}><a href='#home'>Home</a></li>
 				<li className={styles.section}><a href='#about-me'>About me</a></li>
-				{/* <li className={styles.section}><a href="#">My projects</a></li>
-				<li className={styles.section}><a href="#">My skills</a></li> */}
+				<li className={styles.section}><a href="#my-projects">My projects</a></li>
+				{/* <li className={styles.section}><a href="#">My skills</a></li> */}
 			</ul>
 		</div>
 	);
