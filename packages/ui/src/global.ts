5e204f03e574361bbbd02a355e77fe86f2bd990c
valfur03 <valentin.furmanek@gmail.com>
2022-11-07 16:14:25 +0100

    feat: choose the font

--- packages/ui/src/global.ts
+++ packages/ui/src/global.ts
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
 	* {
 		margin: 0;
 		padding: 0;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; // TODO
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 		color: ${props => props.theme.color.onSurface};
 	}
 	
