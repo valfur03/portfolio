1443253e6626e9ba8bfd8ca4257774bbc9ee06f5
valfur03 <valentin.furmanek@gmail.com>
2022-11-09 11:51:25 +0100

    fix(ui): export components

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,1 +1,8 @@
 export { default as Card } from './Card';
+export { default as CardContent } from './CardContent';
+export { default as CardFooter } from './CardFooter';
+export { default as CardMedia } from './CardMedia';
+export { default as GithubRepo } from './GithubRepo';
+export { default as Icon } from './Icon';
+export { default as IconButton } from './IconButton';
+export { default as SkeletonParagraph } from './SkeletonParagraph';
