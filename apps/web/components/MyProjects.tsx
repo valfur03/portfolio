941a2b005a36d7c488a3a5eea1ad089ac819558d
valfur03 <valentin.furmanek@gmail.com>
2022-11-10 20:28:09 +0100

    feat(web): add an image field in the project object

--- apps/web/components/MyProjects.tsx
+++ apps/web/components/MyProjects.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardFooter, CardMedia, GithubRepo } from 'ui';
 const projects = [
 	{
 		name: 'Alekol',
+		image: '/projects/Alekol.webp',
 		description: [
 			'Alekol is a Discord bot to automatically add or remove a role whether you are logged at 42 school. It provides commands that use 42 and Discord API, and uses OAuth2 protocol.',
 			'This is a group project where we used Github Actions and pull requests features.',
@@ -24,9 +25,10 @@ export default function MyProjects() {
 			<h3>My projects</h3>
 			<div className={styles.projects}>
 				{projects.map((project) => {
+					const media = project.image ? <CardMedia image={project.image}/> : null;
 					return (
 						<Card key={project.name}>
-							<CardMedia image={`/projects/${project.name}.webp`}/>
+							{ media }
 							<CardContent label={project.name}>
 								{project.description.map((paragraph, index) => {
 									return <p key={index}>{paragraph}</p>;
