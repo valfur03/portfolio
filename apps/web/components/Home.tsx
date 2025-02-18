2c470d7cc62bf765619e64acedc90939b91d1912
valfur03 <valentin.furmanek@gmail.com>
2025-02-18 16:40:15 +0100

    feat(web): update subtitle to "Software engineer"

--- apps/web/components/Home.tsx
+++ apps/web/components/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 			<div className={styles.favicon}><MeImage /></div>
 			<div className={styles.my_info}>
 				<h1>Valentin Furmanek</h1>
-				<h2>Student at 42Paris.</h2>
+				<h2>Software engineer</h2>
 				<div className={styles.icons_container}>
 					<a className={styles.icon} href='https://github.com/valfur03'><GithubIcon /></a>
 					<a className={styles.icon} href='https://www.linkedin.com/in/valentin-furmanek/'><LinkedinIcon /></a>
