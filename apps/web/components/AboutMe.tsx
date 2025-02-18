f3767b88536d41eb5b7532347bf58fd53372a07c
valfur03 <valentin.furmanek@gmail.com>
2025-02-18 16:41:28 +0100

    feat(web): remove non-sense "libre" word

--- apps/web/components/AboutMe.tsx
+++ apps/web/components/AboutMe.tsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
 			<h3>About me</h3>
 			<div className={styles.paragraphs}>
 				<p className={styles.paragraph}>I like coding since I was 13. I love learning about new technologies, languages, and many other stuffs.</p>
-				<p className={styles.paragraph}>I admire open source, libre and free software projects. This mindset make tech more inclusive according to me.</p>
+				<p className={styles.paragraph}>I admire open source and free software projects. This mindset make tech more inclusive according to me.</p>
 				<p className={styles.paragraph}>I am currently studying at 42 Paris, but if you are interested in working with me, feel free to contact me.</p>
 			</div>
 		</div>
