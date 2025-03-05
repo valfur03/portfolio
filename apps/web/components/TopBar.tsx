5a714cda04c017296b0e4a63c2869983cd7ff773
valfur03 <valentin.furmanek@gmail.com>
2025-03-05 19:56:54 +0100

    feat: replace TopBar links with Blog

--- apps/web/components/TopBar.tsx
+++ apps/web/components/TopBar.tsx
@@ -6,8 +6,7 @@ export default function TopBar() {
 		<div className={styles.top_bar}>
 			<ul className={styles.sections_list}>
 				<li className={styles.section}><a href='#home'>Home</a></li>
-				<li className={styles.section}><a href='#about-me'>About me</a></li>
-				<li className={styles.section}><a href="#my-projects">My projects</a></li>
+				<li className={styles.section}><a href='https://blog.valfur.fr'>Blog</a></li>
 				{/* <li className={styles.section}><a href="#">My skills</a></li> */}
 			</ul>
 		</div>
