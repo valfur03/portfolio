f82416fd5f080785f99e696b626966772836c02c
Valentin Furmanek <valentin.furmanek@strapi.io>
2026-03-22 10:02:34 +0100

    fix(web): use the same style for disabled inputs and textareas

--- apps/web/src/components/Input.tsx
+++ apps/web/src/components/Input.tsx
@@ -7,7 +7,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
 			type={type}
 			data-slot="input"
 			className={cn(
-				"inset-shadow-lg border-none focus-visible:border-ring focus-visible:ring-on-surface/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive disabled:bg-input/50 h-8 rounded border bg-surface px-3 py-4 text-sm transition-colors file:h-6 file:text-xs file:font-medium focus-visible:ring-3 aria-invalid:ring-3 md:text-sm w-full min-w-0 outline-none file:inline-flex file:border-0 file:bg-transparent file:text-on-surface placeholder:text-midgrey disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+				"inset-shadow-lg border-none focus-visible:border-ring focus-visible:ring-on-surface/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive disabled:bg-surface h-8 rounded border bg-surface px-3 py-4 text-sm transition-colors file:h-6 file:text-xs file:font-medium focus-visible:ring-3 aria-invalid:ring-3 md:text-sm w-full min-w-0 outline-none file:inline-flex file:border-0 file:bg-transparent file:text-on-surface placeholder:text-midgrey disabled:cursor-not-allowed disabled:opacity-50",
 				className,
 			)}
 			{...props}
--- apps/web/src/components/TextArea.tsx
+++ apps/web/src/components/TextArea.tsx
@@ -6,7 +6,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
 		<textarea
 			data-slot="textarea"
 			className={cn(
-				"inset-shadow-lg border-none bg-surface focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive disabled:bg-on-surface/50 rounded-lg border px-2.5 py-2 text-base transition-colors focus-visible:ring-3 aria-invalid:ring-3 md:text-sm flex field-sizing-content min-h-16 w-full outline-none placeholder:text-background-secondary disabled:cursor-not-allowed disabled:opacity-50",
+				"inset-shadow-lg border-none bg-surface focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive disabled:bg-surface rounded-lg border px-2.5 py-2 text-base transition-colors focus-visible:ring-3 aria-invalid:ring-3 md:text-sm flex field-sizing-content min-h-16 w-full outline-none placeholder:text-background-secondary disabled:cursor-not-allowed disabled:opacity-50",
 				className,
 			)}
 			{...props}
